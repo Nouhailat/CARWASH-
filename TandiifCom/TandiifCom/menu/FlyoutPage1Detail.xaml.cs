2befd2b80db1dc81b36fd0066b78fb838216d1e3
nouha touati <nouhaila.ribery@gmail.com>
2024-07-03 23:48:36 +0100

    first

---
+++ TandiifCom/TandiifCom/menu/FlyoutPage1Detail.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TandiifCom.menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyoutPage1Detail : ContentPage
+    {
+        public FlyoutPage1Detail()
+        {
+            InitializeComponent();
+        }
+        private async void OnCarouselPositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            var carouselView = sender as CarouselView;
+            var currentView = carouselView.CurrentItem as View;
+
+            if (currentView != null)
+            {
+                currentView.Opacity = 0;
+                await currentView.FadeTo(1, 1000);
+            }
+        }
+    }
+}
---
+++ TandiifCom/TandiifCom/views/LoginPagexaml.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TandiifCom.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPagexaml : ContentPage
+    {
+        public LoginPagexaml()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            // Normally you would perform authentication here (e.g., call a web service)
+            string enteredUsername = usernameEntry.Text;
+            string enteredPassword = passwordEntry.Text;
+
+            // For demonstration, check if username and password match hardcoded values
+            if (enteredUsername == "user" && enteredPassword == "password")
+            {
+                // Navigate to a new page (e.g., MainPage)
+                await Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                // Display an alert if login fails
+                await DisplayAlert("Login Failed", "Invalid username or password", "OK");
+            }
+        }
+    }
+}
